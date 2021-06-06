90d772061fe9f3e4c0a790fa6217c2e6e484423b
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-06-06 16:24:58 +0900

    変更

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-
+//オブジェクト場合
 // const myprofile = {
 //   name: "cherry",
 //   age: 16
@@ -13,3 +13,15 @@ import "./styles.css";
 // const { name, age} = myprofile;
 // const massege2 = `分割代入の名前${name}で${age}歳です`;
 // console.log(massege2);
+
+//配列の場合
+
+const myprofile = ["cherry", 16];
+
+const massege3 = `配列名前${myprofile[0]}で${myprofile[1]}歳です`;
+console.log(massege3);
+//分割代入（配列の場合）
+const [name, age] = myprofile;
+const massege4 = `分割代入の名前${name}で${age}歳です`;
+
+console.log(massege4);
