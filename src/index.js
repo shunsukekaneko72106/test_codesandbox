55418cb32bb4fbef9aa13a5674d2b293a366283e
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-06-06 14:25:26 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const myprofile = {
+//   name: "cherry",
+//   age: 16
+// };
+
+// const massege = `名前${myprofile.name}で${myprofile.age}歳です`;
+// console.log(massege);
+
+// //分割代入
+
+// const { name, age} = myprofile;
+// const massege2 = `分割代入の名前${name}で${age}歳です`;
+// console.log(massege2);
