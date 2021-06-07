06dedd705941a0bd0f8c2fdb2c1c568876b11b8a
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-06-07 20:32:02 +0900

    変数の基礎

--- src/index.js
+++ src/index.js
@@ -16,12 +16,63 @@ import "./styles.css";
 
 //配列の場合
 
-const myprofile = ["cherry", 16];
+// const myprofile = ["cherry", 16];
 
-const massege3 = `配列名前${myprofile[0]}で${myprofile[1]}歳です`;
-console.log(massege3);
-//分割代入（配列の場合）
-const [name, age] = myprofile;
-const massege4 = `分割代入の名前${name}で${age}歳です`;
+// const massege3 = `配列名前${myprofile[0]}で${myprofile[1]}歳です`;
+// console.log(massege3);
+// //分割代入（配列の場合）
+// const [name, age] = myprofile;
+// const massege4 = `分割代入の名前${name}で${age}歳です`;
 
-console.log(massege4);
+// console.log(massege4);
+
+/**
+ *const,let等の変数宣言
+ */
+
+//  var val1 = "val変数";
+//  console.log(val1);
+
+//  //var 変数は上書き可能
+//  val1 = "var変数は上書き可能";
+//  console.log(val1);
+
+//  //var　は変数は再宣言可能
+//  var val1 = "val変数を再宣言";
+//  console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+// >>Identifier 'val2' has already been declared
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// //const変数は上書き不可
+// val3 = "const変数を上書き";
+// >>"val3" is read-only
+
+//const変数は再宣言不可
+// const val3 = "const変数を再宣言";
+
+// //オブジェクトは書き換え可能
+// const val4 = {
+//   name: "cherry",
+//   age: 8
+// }
+// val4.name = "mee";
+// console.log(val4);
+// >>{name: "mee", age: 8}
+
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+// >>(3) ["bird", "cat", "monkey"]
